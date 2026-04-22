a43ae9f90636cc777641940f26ef817f4c9f4316
Alejandro Quintana <alejandro_hernandez_00@hotmail.com>
2026-04-22 20:40:57 -0300

    Lesson 2, variable types

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 {
     static void Main()
     {
-        WriteLine("Hello, World!");
+        int quantity = 5;
+        string message = "Hello World";
+        decimal price = 19.99m;
+        WriteLine($"Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+        //Vars
+        var greeting = "Hola";
+        var percentage = 20.00m;
+        WriteLine($"Saludo: {greeting}, Porcentaje: {percentage}");
     }
 }
